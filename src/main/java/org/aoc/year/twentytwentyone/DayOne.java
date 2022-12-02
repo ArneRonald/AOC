aeedf903f717a98ec1bfab58281a7e270e0e1e8f
Arne Engelbrecht <arne.engelbrecht@pwc.com>
2022-12-02 11:31:12 +0100

    Add day one of 2021

--- src/main/java/org/aoc/year/twentytwentyone/DayOne.java
+++ src/main/java/org/aoc/year/twentytwentyone/DayOne.java
@@ -8,9 +8,40 @@ public class DayOne extends BaseDay {
 
     @Override
     public void solve() {
-        String[] input = retriever.getInputAsStringArray();
-        for (String in: input) {
-            System.out.println(in);
+        int[] input = retriever.getInputAsIntegerArray();
+        int[] fauxInput = new int[] {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+        int result = determineAmountOfIncreases(input);
+        int part2 = determineAmountOfSlidingIncreases(input);
+        System.out.println("--Day one--");
+        System.out.println("Part 1: " + result);
+        System.out.println("Part 2: " + part2);
+    }
+
+    private int determineAmountOfSlidingIncreases(int[] inputs) {
+        int increases = 1;
+        for (int i = 1; i < inputs.length; i++) {
+            if (i == inputs.length - 3){
+                break;
+            }
+            int measurementOne = inputs[i] + inputs[i+1] + inputs[i+2];
+            int measurementTwo = inputs[i+1] + inputs[i+2] + inputs[i+3];
+            increases += isIncreased(measurementTwo, measurementOne);
         }
+        return increases;
+    }
+
+    private int determineAmountOfIncreases(int[] inputs) {
+        int increases = 0;
+        for (int i = 1; i < inputs.length; i++) {
+            increases += isIncreased(inputs[i], inputs[i-1]);
+        }
+        return increases;
+    }
+
+    private int isIncreased(int currentMeasurement, int previousMeasurement) {
+        if (currentMeasurement > previousMeasurement){
+            return 1;
+        }
+        return 0;
     }
 }
